340dd155103855fecd8b6b3a176fb467a187b1ff
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-12-04 00:24:19 +0300

    test counter-main-button

--- src/components/CounterMain/CounterMainButton.test.tsx
+++ src/components/CounterMain/CounterMainButton.test.tsx
@@ -1,33 +1,63 @@
 import React from 'react';
 import {CounterMainButton} from "./CounterMainButton";
-import {render, screen} from "@testing-library/react";
-import {InitialStateType} from "../../state/counter-reducer";
-import {Provider} from "react-redux";
-import {store} from "../../state/store";
+import {fireEvent, render, screen} from "@testing-library/react";
+import * as actions from "../../state/counter-reducer";
+import * as reduxHooks from "react-redux";
+import {AppRootStateType} from "../../state/store";
+
+let startState: AppRootStateType
+
+jest.mock('react-redux')
+
+const mockedDispatch = jest.spyOn(reduxHooks, 'useDispatch')
+const dispatch = jest.fn()
+
+
 describe('test main button', () => {
-    let counter:InitialStateType
     beforeEach(() => {
-        counter = {
+        startState = {
+            counter: {
                 active: false,
-                error:false,
-                isDisabled:true,
-                maxValue:"5",
-                minValue:"0",
-                presentValue:0
-        }
+                error: false,
+                isDisabled: true,
+                maxValue: "5",
+                minValue: "0",
+                presentValue: 0
+            }
+        }}
+    )
+    afterEach(() => {
+        jest.clearAllMocks()
     })
-    test('render button', () => {
-        render(
-            <Provider store={store}>
-                <CounterMainButton counter={counter}/>)
-            </Provider>
-        )
+    it('should create counter main button', () => {
+        mockedDispatch.mockReturnValue(dispatch)
+        const utils = render(<CounterMainButton counter={startState.counter}/>)
+        expect(utils).toMatchSnapshot()
+    })
+    it('render button', () => {
+        render(<CounterMainButton counter={startState.counter}/>)
         const buttonInc = screen.getByText('inc')
         const buttonReset = screen.getByText('reset')
         const buttonAll = screen.getAllByRole('button')
         expect(buttonInc).toBeInTheDocument()
         expect(buttonReset).toBeInTheDocument()
         expect(buttonAll).toEqual([buttonInc, buttonReset])
+    })
 
+    it('should dispatch increment action', () => {
+        mockedDispatch.mockReturnValue(dispatch)
+        const mockedIncrementAC = jest.spyOn(actions, 'IncrementAC')
+        render(<CounterMainButton counter={startState.counter}/>)
+        fireEvent.click(screen.getByText('inc'))
+        expect(dispatch).toHaveBeenCalledTimes(1)
+        expect(mockedIncrementAC).toHaveBeenCalled()
+    })
+    it('should dispatch reset action', () => {
+        mockedDispatch.mockReturnValue(dispatch)
+        const mockedResetAC = jest.spyOn(actions, 'resetAC')
+        render(<CounterMainButton counter={startState.counter}/>)
+        fireEvent.click(screen.getByText('reset'))
+        expect(dispatch).toHaveBeenCalledTimes(1)
+        expect(mockedResetAC).toHaveBeenCalled()
     })
 })
